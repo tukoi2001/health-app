9705a8e490659f4074e83d2bab3165f6755b2af6
Tu Xuan <xuantu14112001@gmail.com>
2023-02-20 14:40:10 +0700

    [Feat]Product UI page

---
+++ src/containers/ProductContainer/ProductContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <DefaultLayout>
+    <Breadcrumbs titleOne="Shop" />
+    <router-view />
+    <TheBanner
+      title="Get help, find answers, download drivers, manuals and much more"
+      description="Contact us online through chat and get support from an expert"
+      btnText="Chat Now"
+    />
+  </DefaultLayout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import Breadcrumbs from "@/components/common/Breadcrumbs.vue";
+import TheBanner from "@/components/common/TheBanner.vue";
+
+export default Vue.extend({
+  name: "ProductContainer",
+  components: {
+    DefaultLayout,
+    Breadcrumbs,
+    TheBanner,
+  },
+  methods: {},
+});
+</script>
---
+++ src/views/Product/ProductPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <ProductContainer />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ProductContainer from "@/containers/ProductContainer/ProductContainer.vue";
+
+export default Vue.extend({
+  name: "ProductPage",
+  components: {
+    ProductContainer,
+  },
+});
+</script>
